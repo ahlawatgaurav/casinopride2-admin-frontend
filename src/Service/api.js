decd219571056df3b41b691704561247909830a6
jmanasi <jmanasi954@gmail.com>
2024-01-17 12:59:59 +0530

    Removed hardcoded authorization

--- src/Service/api.js
+++ src/Service/api.js
@@ -4,28 +4,28 @@ const api = {
   BILLING_PORT: axios.create({
     baseURL: `${process.env.REACT_APP_BASE_URL}${process.env.REACT_APP_BILLING_PORT}/api`,
     headers: {
-      Authorization: "Qm94d1I5MFA6U3BMSlQ1NFFk",
+      Authorization:process.env.AUTHORIZATION,
     },
   }),
 
   BOOKING_PORT: axios.create({
     baseURL: `${process.env.REACT_APP_BASE_URL}${process.env.REACT_APP_BOOKING_PORT}/api`,
     headers: {
-      Authorization: "Qm94d1I5MFA6U3BMSlQ1NFFk",
+      Authorization: process.env.AUTHORIZATION,
     },
   }),
 
   CORE_PORT: axios.create({
     baseURL: `${process.env.REACT_APP_BASE_URL}${process.env.REACT_APP_CORE_PORT}/api`,
     headers: {
-      Authorization: "Qm94d1I5MFA6U3BMSlQ1NFFk",
+      Authorization: process.env.AUTHORIZATION,
     },
   }),
 
   AUTH_PORT: axios.create({
     baseURL: `${process.env.REACT_APP_BASE_URL}${process.env.REACT_APP_AUTH_PORT}/api`,
     headers: {
-      Authorization: "Qm94d1I5MFA6U3BMSlQ1NFFk",
+      Authorization: process.env.AUTHORIZATION,
     },
   }),
 };
--- src/Service/api2.js
+++ src/Service/api2.js
@@ -3,6 +3,6 @@ import axios from "axios";
 export default axios.create({
   baseURL: "http://localhost:9004/api",
   headers: {
-    Authorization: "Qm94d1I5MFA6U3BMSlQ1NFFk",
+    Authorization: process.env.AUTHORIZATION,
   },
 });
